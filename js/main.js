689c5be4f4c0660399be6814a9f5dfd963b0f3ac
AndrewBubnov <andrew.bubnov75@gmail.com>
2018-08-22 13:17:25 +0300

    slider on pure JS

--- js/main.js
+++ js/main.js
@@ -1,3 +1,50 @@
+// let leftButton = document.querySelector(".l-button");
+// let rightButton = document.querySelector(".r-button");
+// let element = document.getElementsByClassName("box");
+// let edgeRight = "right-" + (element.length - 1);
+// let edgeLeft = "right-0";
+//
+// for (let i = 0; i < element.length; i++) {
+//     element[i].classList.add("right-" + i + "");
+// }
+// rightButton.addEventListener('click', function () {
+//
+//     for (let i = element.length - 1; i >= 0 ; i--) {
+//
+//         if (element[i].classList.contains(edgeRight)) {
+//             element[i].classList.remove(edgeRight);
+//             element[i].classList.add(edgeLeft);
+//         } else {
+//             let currentClass = element[i].classList[1];
+//             element[i].classList.remove(currentClass);
+//             element[i].classList.add("right-" + (parseInt(currentClass.slice(-1)) + 1));
+//         }
+//
+//     }
+// });
+//
+//
+// leftButton.addEventListener('click', function () {
+//
+//     for (let i = element.length - 1; i >= 0 ; i--) {
+//
+//         if (element[i].classList.contains(edgeLeft)) {
+//             element[i].classList.remove(edgeLeft);
+//             element[i].classList.add(edgeRight);
+//         } else {
+//             let currentClass = element[i].classList[1];
+//             element[i].classList.remove(currentClass);
+//             element[i].classList.add("right-" + (parseInt(currentClass.slice(-1)) - 1));
+//         }
+//
+//     }
+// });
+//
+
+
+
+
+
 let leftButton = document.querySelector(".l-button");
 let rightButton = document.querySelector(".r-button");
 let element = document.getElementsByClassName("box");
@@ -18,18 +65,17 @@ leftButton.addEventListener('click', function () {
         element[i].style.display = "";
     }
     for (let i = element.length - 1; i >= 0 ; i--) {
-        let currentStyle = element[i].getAttribute("style");
+        let currentStyle = element[i].style;
         scaled(i, currentStyle, 2);
-        if (currentStyle === "transform: translateX(" + (element.length - 1)*(clientWidth + 10) + "px);") {
+        console.log(currentStyle.transform);
+        if (currentStyle.transform === "translateX(" + (element.length - 1)*(clientWidth + 10) + "px)") {
             element[i].style.transform = "";
             element[i].style.display = "none";
             element[i].style.transform = "translateX(0px)";
         } else {
-            let previous = parseInt(currentStyle.match(/\d/g).join(""));
-
+            let previous = parseInt(currentStyle.transform.match(/\d/g).join(""));
             element[i].style.transform = "translateX(" + (previous + clientWidth + 10) + "px)";
         }
-
     }
 });
 
@@ -39,23 +85,22 @@ rightButton.addEventListener('click', function () {
         element[i].style.display = "";
     }
     for (let i = element.length - 1; i >= 0 ; i--) {
-        let currentStyle = element[i].getAttribute("style");
+        let currentStyle = element[i].style;
         scaled(i, currentStyle, 4);
-        if (currentStyle === "transform: translateX(0px);") {
+        if (currentStyle.transform === "translateX(0px)") {
             element[i].style.transform = "";
             element[i].style.display = "none";
             element[i].style.transform = "translateX(" + (element.length - 1)*(clientWidth + 10) + "px)";
         } else {
-            let previous = parseInt(currentStyle.match(/\d/g).join(""));
+            let previous = parseInt(currentStyle.transform.match(/\d/g).join(""));
             element[i].style.transform = "translateX(" + (previous - clientWidth - 10) + "px)";
         }
-
     }
 });
 
 function scaled(i, currentStyle, n) {
     let image = element[i].firstChild;
-    if (currentStyle === "transform: translateX(" + n*(clientWidth + 10) + "px);"){
+    if (currentStyle.transform === "translateX(" + n*(clientWidth + 10) + "px)"){
         image.classList.remove("common-scaled");
         image.classList.add("centered-scaled");
     } else {
