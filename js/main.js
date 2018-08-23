2731b40182eb453ca048c43ceb86fa82b2fa7385
AndrewBubnov <andrew.bubnov75@gmail.com>
2018-08-23 13:25:40 +0300

    slider on pure JS

--- js/main.js
+++ js/main.js
@@ -1,45 +1,96 @@
-// let leftButton = document.querySelector(".l-button");
-// let rightButton = document.querySelector(".r-button");
+
+// let leftButton = document.querySelector("#left");
+// let rightButton = document.querySelector("#right");
 // let element = document.getElementsByClassName("box");
-// let edgeRight = "right-" + (element.length - 1);
-// let edgeLeft = "right-0";
+// let viewWindow = document.querySelector(".view-window");
+// let container = document.querySelector(".container");
+// let wrapper = document.querySelector(".wrapper");
+// let bigPhoto = document.querySelector(".big-photo");
+// let image = element[0].firstChild;
+// let clientWidth = image.clientWidth;
+// let clientHeight = image.clientHeight;
+// let viewWidth = (clientWidth + 10) * 3;
+// viewWindow.style.width = viewWidth + "px";
+// wrapper.style.width = viewWidth + 91 + "px";
+// container.style.height = clientHeight + "px";
+// container.style.width = (clientWidth + 10) * element.length + "px";
+// container.style.marginLeft = -(clientWidth + 10) * 2 + "px";
+//
 //
 // for (let i = 0; i < element.length; i++) {
-//     element[i].classList.add("right-" + i + "");
+//     element[i].style.width = clientWidth + "px";
+//
+//     element[i].style.transform = "translateX(" + i*(clientWidth + 10) + "px)";
+//     let image = element[i].firstChild;
+//     if (i !== 3){
+//         image.classList.add("common-scaled");
+//     } else image.classList.add("centered-scaled");
 // }
-// rightButton.addEventListener('click', function () {
-//
-//     for (let i = element.length - 1; i >= 0 ; i--) {
-//
-//         if (element[i].classList.contains(edgeRight)) {
-//             element[i].classList.remove(edgeRight);
-//             element[i].classList.add(edgeLeft);
-//         } else {
-//             let currentClass = element[i].classList[1];
-//             element[i].classList.remove(currentClass);
-//             element[i].classList.add("right-" + (parseInt(currentClass.slice(-1)) + 1));
-//         }
-//
-//     }
-// });
-//
 //
 // leftButton.addEventListener('click', function () {
-//
+//     for (let i = 0; i < element.length; i++) {
+//         element[i].style.display = "";
+//     }
 //     for (let i = element.length - 1; i >= 0 ; i--) {
-//
-//         if (element[i].classList.contains(edgeLeft)) {
-//             element[i].classList.remove(edgeLeft);
-//             element[i].classList.add(edgeRight);
+//         let currentStyle = element[i].style;
+//         scaled(i, currentStyle, 2);
+//         if (currentStyle.transform === "translateX(" + (element.length - 1)*(clientWidth + 10) + "px)") {
+//             element[i].style.transform = "";
+//             element[i].style.display = "none";
+//             element[i].style.transform = "translateX(0px)";
 //         } else {
-//             let currentClass = element[i].classList[1];
-//             element[i].classList.remove(currentClass);
-//             element[i].classList.add("right-" + (parseInt(currentClass.slice(-1)) - 1));
+//             let previous = parseInt(currentStyle.transform.match(/\d/g).join(""));
+//             element[i].style.transform = "translateX(" + (previous + clientWidth + 10) + "px)";
 //         }
-//
 //     }
 // });
 //
+//
+// rightButton.addEventListener('click', function () {
+//     for (let i = 0; i < element.length; i++) {
+//         element[i].style.display = "";
+//     }
+//     for (let i = element.length - 1; i >= 0 ; i--) {
+//         let currentStyle = element[i].style;
+//         scaled(i, currentStyle, 4);
+//         if (currentStyle.transform === "translateX(0px)") {
+//             element[i].style.transform = "";
+//             element[i].style.display = "none";
+//             element[i].style.transform = "translateX(" + (element.length - 1)*(clientWidth + 10) + "px)";
+//         } else {
+//             let previous = parseInt(currentStyle.transform.match(/\d/g).join(""));
+//             element[i].style.transform = "translateX(" + (previous - clientWidth - 10) + "px)";
+//         }
+//     }
+// });
+//
+// function scaled(i, currentStyle, n) {
+//     let image = element[i].firstChild;
+//     if (currentStyle.transform === "translateX(" + n*(clientWidth + 10) + "px)"){
+//         image.classList.remove("common-scaled");
+//         image.classList.add("centered-scaled");
+//     } else {
+//         image.classList.remove("centered-scaled");
+//         image.classList.add("common-scaled");
+//     }
+// }
+//
+//
+// for (let i = 0; i < element.length; i++) {
+//     element[i].addEventListener("click", function(e) {
+//         let target = e.target.parentNode;
+//         target.parentNode.classList.toggle('active');
+//         for (let j = 0; j < element.length; j++) {
+//             if (element[j] !== target.parentNode && element[j].classList.contains('active')){
+//                 element[j].classList.remove('active');
+//             }
+//         }
+//         if (!target.parentNode.classList.contains('active')){
+//             bigPhoto.innerHTML = "<div class='green-circle minify'><img class='clip-circle ' src = 'img/photo-" + (i + 1) + ".jpg'></div>";
+//         }
+//         else bigPhoto.innerHTML = "<div class='green-circle enlarge'><img class='clip-circle ' src = 'img/photo-" + (i + 1) + ".jpg'></div>";
+//     });
+// }
 
 
 
@@ -51,20 +102,24 @@ let element = document.getElementsByClassName("box");
 let viewWindow = document.querySelector(".view-window");
 let container = document.querySelector(".container");
 let wrapper = document.querySelector(".wrapper");
+let bigPhoto = document.querySelector(".big-photo");
+let innerWrapper = document.querySelector(".inner-wrapper");
 let image = element[0].firstChild;
 let clientWidth = image.clientWidth;
 let clientHeight = image.clientHeight;
 let viewWidth = (clientWidth + 10) * 3;
+let array = [];
 viewWindow.style.width = viewWidth + "px";
 wrapper.style.width = viewWidth + 91 + "px";
-container.style.height = clientHeight + "px";
+innerWrapper.style.height = clientHeight + "px";
 container.style.width = (clientWidth + 10) * element.length + "px";
 container.style.marginLeft = -(clientWidth + 10) * 2 + "px";
-
+bigPhoto.style.width = clientWidth + "px";
+bigPhoto.style.height = clientHeight + "px";
 
 for (let i = 0; i < element.length; i++) {
     element[i].style.width = clientWidth + "px";
-
+    array[i] = i;
     element[i].style.transform = "translateX(" + i*(clientWidth + 10) + "px)";
     let image = element[i].firstChild;
     if (i !== 3){
@@ -79,11 +134,13 @@ leftButton.addEventListener('click', function () {
     for (let i = element.length - 1; i >= 0 ; i--) {
         let currentStyle = element[i].style;
         scaled(i, currentStyle, 2);
-        if (currentStyle.transform === "translateX(" + (element.length - 1)*(clientWidth + 10) + "px)") {
+        if (array[i] === element.length - 1) {
+            array[i] = 0;
             element[i].style.transform = "";
             element[i].style.display = "none";
             element[i].style.transform = "translateX(0px)";
         } else {
+            array[i]++;
             let previous = parseInt(currentStyle.transform.match(/\d/g).join(""));
             element[i].style.transform = "translateX(" + (previous + clientWidth + 10) + "px)";
         }
@@ -98,11 +155,13 @@ rightButton.addEventListener('click', function () {
     for (let i = element.length - 1; i >= 0 ; i--) {
         let currentStyle = element[i].style;
         scaled(i, currentStyle, 4);
-        if (currentStyle.transform === "translateX(0px)") {
+        if (array[i] === 0) {
+            array[i] = element.length - 1;
             element[i].style.transform = "";
             element[i].style.display = "none";
             element[i].style.transform = "translateX(" + (element.length - 1)*(clientWidth + 10) + "px)";
         } else {
+            array[i]--;
             let previous = parseInt(currentStyle.transform.match(/\d/g).join(""));
             element[i].style.transform = "translateX(" + (previous - clientWidth - 10) + "px)";
         }
@@ -118,4 +177,27 @@ function scaled(i, currentStyle, n) {
         image.classList.remove("centered-scaled");
         image.classList.add("common-scaled");
     }
+}
+
+let shown = false;
+for (let i = 0; i < element.length; i++) {
+    element[i].addEventListener("click", function() {
+
+        if (array[i] === 2){
+            leftButton.click();
+            shown = false;
+        } else if (array[i] === 4) {
+            rightButton.click();
+            shown = false;
+        }
+
+        if (shown === true){
+            bigPhoto.innerHTML = "<div class='green-circle minify'><img class='clip-circle ' src = 'img/photo-" + (i + 1) + ".jpg'></div>";
+            shown = false;
+        }
+        else {
+            bigPhoto.innerHTML = "<div class='green-circle enlarge'><img class='clip-circle ' src = 'img/photo-" + (i + 1) + ".jpg'></div>";
+            shown = true;
+        }
+    });
 }
